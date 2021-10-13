7fd58b8040a5598d133ceaa9d8b084ed5f1b3f4b
Elbuagnin <elbuagnin@gmail.com>
2021-10-12 22:58:48 -0500

    more normalization

--- pre-parser.js
+++ pre-parser.js
@@ -25,8 +25,35 @@ export default function preParser(document) {
     });
   }
 
+  function tagHyphenatedTerms() {
+    const hyphenatedTerms = document.hyphenated();
+    hyphenatedTerms.tag(['#Noun', 'Singular', 'Hyphenated']);
+  }
+
+  function tagParentheses() {
+    const parenthesesGroups = document.parentheses();
+    parenthesesGroups.firstTerms().tag('BEGIN');
+    parenthesesGroups.lastTerms().tag('END');
+    parenthesesGroups.tag('ParenthesesGroup');
+  }
+
+  function tagQuotations() {
+    const quotationGroups = document.quotations();
+    quotationGroups.firstTerms().tag('BEGIN');
+    quotationGroups.lastTerms().tag('END');
+    quotationGroups.tag('QuotationGroup');
+  }
+
+  // Custom tags & words
   const rulePath = './rules/pre-parser/';
   const dataset = mfs.loadJSONDir(rulePath);
 
+  // Normalize the document for parsing.
+  document.contractions().expand();
+  tagHyphenatedTerms();
   assignValues(dataset);
+  document.lists().tag('List');
+  tagParentheses();
+  tagQuotations();
+  document.if('@hasDash').tag('SPLIT');
 }
