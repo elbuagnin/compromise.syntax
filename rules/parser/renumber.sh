0da83747cefb0dab47dccd9a77eac09ffd82b2c7
elbuagnin <elbuagnin@gmail.com>
2021-10-24 09:38:10 -0500

    streamlined

--- rules/parser/renumber.sh
+++ rules/parser/renumber.sh
@@ -1,24 +1,6 @@
 #!/bin/bash
 ### Renumber the order # in a Parser JSON file.
 
-# echo text niceties
-bold=$(tput bold)
-normal=$(tput sgr0)
-
-# prompt function
-confirm() {
-  while true; do
-    read -r -n 1 -p "${1:-Continue?} [y/n]: " REPLY
-    case $REPLY in
-      [yY]) echo ; return 0 ;;
-      [nN]) echo ; return 1 ;;
-      *) printf " \033[31m %s \n\033[0m" "invalid input"
-    esac
-  done
-}
-
-confirm "Renumber the rules order in file: $1 ?" || exit 0
-
 linenum=0
 i=1
 
