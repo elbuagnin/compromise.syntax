a8ff866fdeb3f436a50ed15a4d2e40672283148a
Elbuagnin <elbuagnin@gmail.com>
2022-04-17 09:23:55 -0500

    removed all obsolete preparser

--- initialize.js
+++ initialize.js
@@ -1,38 +1,5 @@
 import './compromise-extensions.js';
-import preParser from './pre-parser.js';
 import * as mfs from './lib/filesystem.js';
-
-function preTagCorrecter(doc) {
-  // Adverbs mixed with Verbs are marked Auxiliary which interferes with Verbal tags.
-  const adverbs = doc.match('(#Auxiliary && #Adverb)');
-
-  if (adverbs) {
-    console.log(`AAAAAAAAAAAAAAAAAAAA ${JSON.stringify(adverbs.json('tags'))}`);
-    adverbs.untag('#Auxiliary');
-    console.log(`AAAAAAAAAAAAAAAAAAAA ${JSON.stringify(adverbs.json('tags'))}`);
-  }
-}
-
-function postTagCorrecter(doc) {
-  // This module uses custom tags that run counter to some of Compromise's tags.
-  // As such, some custom tags can confuse Compromise's internal tagger.
-  // This subroutine fixes these tags after the custom tags are applied.
-
-  // Untagged Verbs
-  const emptyTag = doc.not('(#Noun|#Verb|#Value|#Date|#Adjective|#Contraction|#Adverb|#Currency|#Determiner|#Conjunction|#Preposition|#Possessive|#QuestionWord|#Pronoun|#Expression|#Abbreviation|#Url|#HashTag|#PhoneNumber|#AtMention|#Emoji|#Emoticon|#Email|#Auxiliary|#Negative|#Acronym|#Verbal)');
-
-  emptyTag.tag(['#BaseVerb', 'PresentTense']);
-
-  // Verbs with no tense
-  const noTenseVerbs = doc.match('(#Verb && !#PresentTense && !#Infinitive && !#Gerund && !#PastTense && !#PerfectTense && !#FuturePerfect && !#Pluperfect && !#Copula && !#Modal && !#Participle && !#Particle && !#PhrasalVerb)');
-
-  noTenseVerbs.tag('#PresentTense');
-
-  // Gerunds turned into Auxiliary Verbs, but need to be Verbals
-  const gerunds = doc.match('(#Auxiliary && #Verb && /ing$/)');
-
-  gerunds.tag(['#ProgressiveVerbal', '#Verbal']);
-}
 
 export default function initialize(doc) {
   const baseDir = './rules/initialize/';
@@ -44,16 +11,9 @@ export default function initialize(doc) {
 
   console.log('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$');
   console.log('$$ Initialization');
-  preTagCorrecter(doc);
   doc.addCustomTags(tags);
   doc.addCustomWords(words);
-  postTagCorrecter(doc);
 
   console.log('Doc, post-Initialization');
   console.log(doc.debug());
-
-  console.log('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$');
-  console.log('$$ Pre-Parser');
-
-  preParser(doc);
 }
