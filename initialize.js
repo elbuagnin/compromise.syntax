612774c1bc6b5dd2decdd72c09e6c174661813da
Elbuagnin <elbuagnin@gmail.com>
2022-11-19 18:54:59 -0600

    ground data path to mod

--- initialize.js
+++ initialize.js
@@ -1,28 +1,28 @@
 import nlp from "compromise";
 import disambiguation from "compromise.disambiguation";
+import path from "path";
+import { fileURLToPath } from "url";
 import "./compromise-extensions.js";
 import * as mfs from "./lib/filesystem.js";
 
 export default function initialize(doc) {
-
   nlp.plugin(disambiguation);
-
-  // const primeTheEngine = nlp('prime');
-  // primeTheEngine.disambiguate();
-
   doc.disambiguate();
 
-  const baseDir = "./rules/initialize/";
-  const tagDir = `${baseDir}tags/`;
-  const wordDir = `${baseDir}words/`;
+  const here = fileURLToPath(new URL(".", import.meta.url));
+  const initializeDir = "./rules/initialize/";
+  const initDirPath = path.join(here, initializeDir);
+
+  const tagDir = path.join(initDirPath, 'tags/');
+  const wordDir = path.join(initDirPath, 'words/');
 
   const tags = mfs.loadJSONDir(tagDir, "tags");
   const words = mfs.loadJSONDir(wordDir, "words");
 
   console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
   console.log("$$ Initialization");
-  doc.addCustomTags(tags);
-  doc.addCustomWords(words);
+  nlp.addTags(tags);
+  nlp.addWords(words);
 
   console.log("Doc, post-Initialization");
   console.log(doc.debug());
